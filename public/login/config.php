c80c56ca04c25c7dadae66796a1bb762720a07eb
Brodie Berger <40439538+brodieberger@users.noreply.github.com>
2024-11-26 18:58:58 -0500

    Update config.php

--- public/login/config.php
+++ public/login/config.php
@@ -1,5 +1,7 @@
 <?php
 // Database configuration
+
+//This file should be deleted and set to be ignored by git. It has sensitive database information.
 $host = "127.0.0.1:3307";
 $username = "root";
 $password = "";
